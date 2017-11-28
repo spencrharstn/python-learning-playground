089438dae90fc6cf98320cda600c1acbedca6b4a
Spencer Harston <spencer314@gmail.com>
2017-11-27 19:11:26 -0700

    Clean up palindrome.py
    Better naming of variables, use enumerate instead of range() and len(), clean up code

--- playground/palindrome.py
+++ playground/palindrome.py
@@ -1,15 +1,15 @@
-# Palindrome
+# palindrome.py
 # Takes a string to see if it is a valid palindrome
 
 import re
 
 def reverse(input_string):
-	reverse = []
+	reverse_string = []
 
 	for letter in range(len(input_string) - 1, 0-1, -1):
-		reverse.append(input_string[letter])
+		reverse_string.append(input_string[letter])
 
-	result = "".join(reverse)
+	result = "".join(reverse_string)
 	return result
 
 def palindrome(input_string):
@@ -19,23 +19,22 @@ def palindrome(input_string):
 	# Get the reversed string
 	reverse_string = reverse(input_string)
 
-	for letter in range(len(input_string)):
-		#print("\tinput: ", input_string[letter], "\treverse: ", reverse_string[letter])
-		if (input_string[letter] != reverse_string[letter]):
+	for i, letter in enumerate(input_string):
+		if letter != reverse_string[i]:
 			return False
 	return True
 
-string = input("Enter a string: ")
+user_string = input("Enter a string: ")
 
-# Read a palindrome from a text file
+# Alternatively, read a palindrome from a text file
 # file = open('palindrome.txt', 'r')
-# string = file.read()
+# user_string = file.read()
 # file.close()
 
-print("\nString:\n\t", string)
-print("\nReversed:\n\t", reverse(string))
+print("\nString:\n\t", user_string)
+print("\nReversed:\n\t", reverse(user_string))
 
-if (palindrome(string)):
+if palindrome(user_string):
 	print('The string is a palindrome!')
 else:
 	print('The string is not a palindrome!')
